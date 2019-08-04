965570c089fa1d7c3bc420498aa271816894f8cb
Agar3s <ekeisco@gmail.com>
2019-08-04 13:46:51 -0500

    last merge before mx

--- Dungeon.gd
+++ Dungeon.gd
@@ -7,9 +7,9 @@ var Milton = preload("res://MonsterMilton.tscn")
 func next_monster():
 	randomize()
 	var value = randf()
-	if value < 0.05:
+	if value < 0.5:
 		return Monster.instance()
-	if value < 0.9:
+	if value < 0.75:
 		return Milton.instance()
 	else:
 		return Marta.instance()
